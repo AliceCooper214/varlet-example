59700877466c5c11e3d5253c466cee3c1d55b6a2
haoziqaq <357229046@qq.com>
2022-04-01 01:11:01 +0800

    optimize: optimize entry

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,7 @@
+<script setup lang="ts">
+const msg = $ref('hello varlet!')
+</script>
+
 <template>
-  <div class="app-container">
-    <div class="project-container">
-      <img class="logo" src="./assets/logo.png" alt="logo" />
-      <div class="name">VARLET</div>
-    </div>
-    <hello-world msg="Material design mobile component library for Vue3" />
-  </div>
+  <var-button type="primary">{{ msg }}</var-button>
 </template>
-
-<style lang="less">
-@keyframes fade-in {
-  from {
-    transform: scale(0.9);
-    opacity: .1;
-  }
-
-  to {
-    transform: scale(1);
-    opacity: 1;
-  }
-}
-
-* {
-  box-sizing: border-box;
-}
-
-body {
-  margin: 0;
-  padding: 0;
-}
-
-.app-container {
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  min-height: 100vh;
-  animation: fade-in 1s forwards;
-
-  .project-container {
-    display: flex;
-    align-items: center;
-
-    .logo {
-      width: 36px;
-      margin-right: 16px;
-    }
-
-    .name {
-      font-size: 32px;
-      color: #555;
-    }
-  }
-}
-</style>
